b261770b0fff9f7ecbbe2af287a4a4d464bdf285
rémi olivan <remi@MacBook-Air-de-remi.local>
2026-08-20 12:38:47 +0400

    Onboarding, cancellation, OAuth sign-in

--- src/lib/constants.js
+++
@@ -1,7 +0,0 @@
-export const DEVELOPERS = [
-  'Emaar', 'Damac', 'Aldar', 'Sobha', 'Meraas', 'Azizi', 'Binghatti', 'Ellington',
-  'Arada', 'Nakheel', 'Samana', 'Reportage', 'Imtiaz', 'Alef', 'Nshama',
-  'Object One', 'Danube', 'MAF', 'Tiger', 'Beyond', 'Other',
-]
-
-export const BEDROOM_OPTIONS = ['Studio', '1BR', '2BR', '3BR', '4BR', '5+BR']
